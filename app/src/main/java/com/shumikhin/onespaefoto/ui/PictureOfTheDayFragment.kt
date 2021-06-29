a899772a69e14715ba335e2838d2dda8e43d3dfa
sergey <sashumihin@gmail.com>
2021-06-29 18:18:05 +0300

    1й урок. 11 стр.

--- app/src/main/java/com/shumikhin/onespaefoto/ui/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/shumikhin/onespaefoto/ui/PictureOfTheDayFragment.kt
@@ -1,16 +1,21 @@
 package com.shumikhin.onespaefoto.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import coil.api.load
 import com.shumikhin.onespaefoto.R
+import com.shumikhin.onespaefoto.databinding.MainFragmentBinding
 import com.shumikhin.onespaefoto.ui.picture.PictureOfTheDayData
 
 class PictureOfTheDayFragment : Fragment() {
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
 
     //Ленивая инициализация модели
     private val viewModel: PictureOfTheDayViewModel by lazy {
@@ -21,8 +26,21 @@ class PictureOfTheDayFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        //return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.getRoot()
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // по нажатию на иконку переходим в браузер и открывать нужную страницу в «Википедии»
+        binding.inputLayout.setEndIconOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
+            })
+        }
+    }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -35,14 +53,14 @@ class PictureOfTheDayFragment : Fragment() {
                 val serverResponseData = data.serverResponseData
                 val url = serverResponseData.url
                 if (url.isNullOrEmpty()) {
-                //Отобразите ошибку
-                //showError("Сообщение, что ссылка пустая")
+                    //Отобразите ошибку
+                    //showError("Сообщение, что ссылка пустая")
                 } else {
                     //Отобразите фото
                     //showSuccess()
                     //Coil в работе: достаточно вызвать у нашего ImageView
                     //нужную extension-функцию и передать ссылку и заглушки для placeholder
-                    image_view.load(url) {
+                    binding.imageView.load(url) {
                         lifecycle(this@PictureOfTheDayFragment)
                         error(R.drawable.ic_load_error_vector)
                         placeholder(R.drawable.ic_no_photo_vector)
@@ -50,8 +68,8 @@ class PictureOfTheDayFragment : Fragment() {
                 }
             }
             is PictureOfTheDayData.Loading -> {
-            //Отобразите загрузку
-            //showLoading()
+                //Отобразите загрузку
+                //showLoading()
             }
             is PictureOfTheDayData.Error -> {
             }
