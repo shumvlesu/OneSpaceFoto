ba4116a3f5b1b19cc6b26995992f3aeceb393f5d
sergey <sashumihin@gmail.com>
2021-06-29 16:05:56 +0300

    1й урок. 8 стр.

---
+++ app/src/main/java/com/shumikhin/onespaefoto/ui/PictureOfTheDayFragment.kt
@@ -0,0 +1,60 @@
+package com.shumikhin.onespaefoto.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.shumikhin.onespaefoto.R
+import com.shumikhin.onespaefoto.ui.picture.PictureOfTheDayData
+
+class PictureOfTheDayFragment : Fragment() {
+
+    //Ленивая инициализация модели
+    private val viewModel: PictureOfTheDayViewModel by lazy {
+        ViewModelProviders.of(this).get(PictureOfTheDayViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.getData().observe(this@PictureOfTheDayFragment, Observer<PictureOfTheDayData> { renderData(it) })
+    }
+
+    private fun renderData(data: PictureOfTheDayData) {
+        when (data) {
+            is PictureOfTheDayData.Success -> {
+                val serverResponseData = data.serverResponseData
+                val url = serverResponseData.url
+                if (url.isNullOrEmpty()) {
+                //Отобразите ошибку
+                //showError("Сообщение, что ссылка пустая")
+                } else {
+                    //Отобразите фото
+                    //showSuccess()
+                    //Coil в работе: достаточно вызвать у нашего ImageView
+                    //нужную extension-функцию и передать ссылку и заглушки для placeholder
+                    image_view.load(url) {
+                        lifecycle(this@PictureOfTheDayFragment)
+                        error(R.drawable.ic_load_error_vector)
+                        placeholder(R.drawable.ic_no_photo_vector)
+                    }
+                }
+            }
+            is PictureOfTheDayData.Loading -> {
+            //Отобразите загрузку
+            //showLoading()
+            }
+            is PictureOfTheDayData.Error -> {
+            }
+        }
+    }
+}
