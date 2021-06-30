647e037adf0f6afc6e3778345e5b57fdbd77ad97
sergey <sashumihin@gmail.com>
2021-06-30 16:07:09 +0300

    1й урок. 17 стр.

--- app/src/main/java/com/shumikhin/onespaefoto/MainActivity.kt
+++ app/src/main/java/com/shumikhin/onespaefoto/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, PictureOfTheDayFragment())
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance())
                 .commitNow()
         }
     }
--- app/src/main/java/com/shumikhin/onespaefoto/ui/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/shumikhin/onespaefoto/ui/PictureOfTheDayFragment.kt
@@ -4,18 +4,24 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import coil.api.load
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.shumikhin.onespaefoto.MainActivity
 import com.shumikhin.onespaefoto.R
 import com.shumikhin.onespaefoto.databinding.MainFragmentBinding
+import com.shumikhin.onespaefoto.ui.picture.BottomNavigationDrawerFragment
 import com.shumikhin.onespaefoto.ui.picture.PictureOfTheDayData
 
 class PictureOfTheDayFragment : Fragment() {
 
     private var _binding: MainFragmentBinding? = null
     private val binding get() = _binding!!
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
     //Ленивая инициализация модели
     private val viewModel: PictureOfTheDayViewModel by lazy {
@@ -39,6 +45,38 @@ class PictureOfTheDayFragment : Fragment() {
                 data = Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
             })
         }
+        setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
+        setBottomAppBar(view)
+    }
+
+    //метод, в который будем передавать наш BottomSheet и инициализировать bottomSheetBehavior.
+    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED //В свернутое состояние, но не скрытое
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.app_bar_fav -> Toast.makeText(context, "Favourite", Toast.LENGTH_SHORT).show()
+            R.id.app_bar_search -> Toast.makeText(context, "Search", Toast.LENGTH_SHORT).show()
+            android.R.id.home -> {
+                activity?.let {
+                    BottomNavigationDrawerFragment().show(it.supportFragmentManager, "tag")
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setBottomAppBar(view: View) {
+        val context = activity as MainActivity
+        context.setSupportActionBar(view.findViewById(R.id.bottom_app_bar))
+        setHasOptionsMenu(true)
     }
 
 
@@ -74,5 +112,10 @@ class PictureOfTheDayFragment : Fragment() {
             is PictureOfTheDayData.Error -> {
             }
         }
+    }
+
+    companion object {
+        fun newInstance() = PictureOfTheDayFragment()
+        private var isMain = true
     }
 }
