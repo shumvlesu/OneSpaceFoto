77f895fff6638c2037b3e3a203705875b3d01974
sergey <sashumihin@gmail.com>
2021-07-12 09:14:20 +0300

    3е ДЗ Начало. Надо сделать привязку в активити.

--- app/src/main/java/com/shumikhin/onespaefoto/ui/api/ApiActivity.kt
+++ app/src/main/java/com/shumikhin/onespaefoto/ui/api/ApiActivity.kt
@@ -1,32 +1,19 @@
 package com.shumikhin.onespaefoto.ui.api
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.shumikhin.onespaefoto.R
+import androidx.appcompat.app.AppCompatActivity
 import com.shumikhin.onespaefoto.databinding.ActivityApiBinding
-import com.shumikhin.onespaefoto.databinding.MainFragmentBinding
 
 class ApiActivity : AppCompatActivity() {
 
     private var _binding: ActivityApiBinding? = null
     private val binding get() = _binding!!
 
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        _binding = ActivityApiBinding.inflate(inflater, container, false)
-//        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
-//        return binding.getRoot()
-//    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_api)
-        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
+        _binding = ActivityApiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewPagerAdapter(supportFragmentManager).also { binding.viewPager.adapter = it }
     }
 
 }
