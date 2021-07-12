f87cddf2918d8acb4af97724314acd4f42271a44
sergey <sashumihin@gmail.com>
2021-07-12 16:55:08 +0300

    3е ДЗ Начало. Надо сделать привязку в активити.

--- app/src/main/java/com/shumikhin/onespaefoto/ui/api/ApiActivity.kt
+++ app/src/main/java/com/shumikhin/onespaefoto/ui/api/ApiActivity.kt
@@ -1,6 +1,7 @@
 package com.shumikhin.onespaefoto.ui.api
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.shumikhin.onespaefoto.R
 import com.shumikhin.onespaefoto.databinding.ActivityApiBinding
@@ -19,10 +20,19 @@ class ApiActivity : AppCompatActivity() {
             //заполняем TabLayout, используется getPageTitle() из ViewPagerAdapter
             binding.tabLayout.setupWithViewPager(binding.viewPager)
             //Добавим иконки на наши вкладки
-            binding.tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_earth)
-            binding.tabLayout.getTabAt(1)?.setIcon(R.drawable.ic_mars)
-            binding.tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_system)
+//            binding.tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_earth)
+//            binding.tabLayout.getTabAt(1)?.setIcon(R.drawable.ic_mars)
+//            binding.tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_system)
         }
+        setCustomTabs()
+    }
+
+    //Для каждой вкладки будет свой макет с соответствующей подписью и иконкой.
+    private fun setCustomTabs() {
+        val layoutInflater = LayoutInflater.from(this)
+        binding.tabLayout.getTabAt(0)?.customView = layoutInflater.inflate(R.layout.activity_api_custom_tab_earth, null)
+        binding.tabLayout.getTabAt(1)?.customView = layoutInflater.inflate(R.layout.activity_api_custom_tab_mars, null)
+        binding.tabLayout.getTabAt(2)?.customView = layoutInflater.inflate(R.layout.activity_api_custom_tab_weather,null)
     }
 
 }
