109e9647b5cc44726ee9a271bcb626c059b6389a
sergey <sashumihin@gmail.com>
2021-07-25 15:08:59 +0300

    5е ДЗ. Стр. 18

--- app/src/main/java/com/shumikhin/onespaefoto/ui/animations/AnimationsActivity.kt
+++ app/src/main/java/com/shumikhin/onespaefoto/ui/animations/AnimationsActivity.kt
@@ -1,56 +1,154 @@
 package com.shumikhin.onespaefoto.ui.animations
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.view.Gravity
-import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
 import androidx.transition.*
-import com.shumikhin.onespaefoto.databinding.ActivityAnimationsEnlargeBinding
-import com.shumikhin.onespaefoto.databinding.ActivityAnimationsPathTransitionsBinding
-import com.shumikhin.onespaefoto.databinding.ActivityAnimationsShuffleBinding
+import com.shumikhin.onespaefoto.databinding.ActivityAnimationsFabBinding
 
 class AnimationsActivity : AppCompatActivity() {
 
-    private var _binding: ActivityAnimationsShuffleBinding? = null
+    private var _binding: ActivityAnimationsFabBinding? = null
     private val binding get() = _binding!!
+    private var isExpanded = false
 
+    //Такой способ анимации объектов устарел: он очень многословен, всё нужно делать вручную.
+    //Но он всё ещё не потерял своей эффективности и, возможно, ещё сможет вам пригодиться для
+    //каких-то простых анимаций или поддержки старых проектов.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityAnimationsShuffleBinding.inflate(layoutInflater)
+        _binding = ActivityAnimationsFabBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setFAB()
+    }
 
-        //В методе createViews мы программно создаём TextView и добавляем в LinearLayout. Этот
-        //процесс вам должен быть знаком по первому курсу. В методе onCreate мы создаём список текста,
-        //который мы будем перемешивать. Далее в TransitionManager мы передаём контейнер, в котором
-        //нужно анимировать элементы. В нашем случае это LinearLayout, который содержит динамически
-        //изменяемый набор из TextView. Как только в контейнере меняется набор элементов,
-        //TransitionManager смотрит, совпадают ли уникальные id новых TextView со старыми. Если да, то
-        //происходит плавное перемещение старого элемента на новое место.
-        //Этот способ подходит не только для TextView, но и для любого другого виджета.
-        val titles: MutableList<String> = ArrayList()
-        for (i in 0..4) {
-            titles.add(String.format("Item %d", i + 1))
-        }
-        createViews(binding.transitionsContainer, titles)
-        binding.button.setOnClickListener {
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer,
-                ChangeBounds())
-            titles.shuffle()
-            createViews(binding.transitionsContainer, titles)
+    private fun setFAB() {
+        setInitialState()
+        binding.fab.setOnClickListener {
+            if (isExpanded) {
+                collapseFab()
+            } else {
+                expandFAB()
+            }
         }
     }
-    private fun createViews(layout: ViewGroup, titles: List<String>) {
-        layout.removeAllViews()
-        for (title in titles) {
-            val textView = TextView(this)
-            textView.text = title
-            textView.gravity = Gravity.CENTER_HORIZONTAL
-            ViewCompat.setTransitionName(textView, title)
-            layout.addView(textView)
+
+    private fun setInitialState() {
+        binding.transparentBackground.apply {
+            alpha = 0f
         }
+        binding.optionTwoContainer.apply {
+            alpha = 0f
+            isClickable = false
+        }
+        binding.optionOneContainer.apply {
+            alpha = 0f
+            isClickable = false
+        }
+    }
+
+    private fun expandFAB() {
+        isExpanded = true
+
+        // Вызываем метод ObjectAnimator ofFloat, который позволяет изменять состояние виджета
+        //от одного значения (во float) до другого.
+        // Последовательно мы передаём в этот метод сначала иконку с параметром "rotation". В
+        //следующем параметре указываем, на сколько градусов по часовой стрелке мы хотим
+        //провернуть иконку
+        ObjectAnimator.ofFloat(binding.plusImageview, "rotation", 0f, 225f).start()
+        //Затем передаём контейнеры с опциями и параметрами "translationY" — это движение по
+        //вертикали вверх. Первый контейнер будет подниматься чуть выше второго.
+        ObjectAnimator.ofFloat(binding.optionTwoContainer, "translationY", -130f).start()
+        ObjectAnimator.ofFloat(binding.optionOneContainer, "translationY",-250f).start()
+
+        // Изменяем свойства контейнеров с помощью метода animate: меняем прозрачность 0 на 1 с
+        //длительностью в 300 миллисекунд и вешаем слушатель конца анимации. Когда анимация
+        //закончится, мы сделаем контейнеры кликабельными и повесим соответствующие слушатели
+        //нажатий.
+        binding.optionTwoContainer.animate()
+            .alpha(1f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    // Изменяем свойство затенения: делаем его непрозрачным и кликабельным. Это позволяет нам
+                    //эффективно запретить взаимодействие с любыми элементами, находящимися под
+                    //затенением.
+                    //При повторном нажатии осуществляем все анимации с точностью до наоборот
+                    binding.optionTwoContainer.isClickable = true
+                    binding.optionTwoContainer.setOnClickListener {
+                        Toast.makeText(this@AnimationsActivity, "Option 2",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+
+        binding.optionOneContainer.animate()
+            .alpha(1f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    binding.optionOneContainer.isClickable = true
+                    binding.optionOneContainer.setOnClickListener {
+                        Toast.makeText(this@AnimationsActivity, "Option 1",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+
+        binding.transparentBackground.animate()
+            .alpha(0.9f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    binding.transparentBackground.isClickable = true
+                }
+            })
+    }
+
+    private fun collapseFab() {
+        isExpanded = false
+        // Вызываем метод ObjectAnimator ofFloat, который позволяет изменять состояние виджета
+        //от одного значения (во float) до другого.
+        // Последовательно мы передаём в этот метод сначала иконку с параметром "rotation". В
+        //следующем параметре указываем, на сколько градусов по часовой стрелке мы хотим
+        //провернуть иконку
+        ObjectAnimator.ofFloat(binding.plusImageview, "rotation", 0f, -180f).start()
+        //Затем передаём контейнеры с опциями и параметрами "translationY" — это движение по
+        //вертикали вверх. Первый контейнер будет подниматься чуть выше второго.
+        ObjectAnimator.ofFloat(binding.optionTwoContainer, "translationY", 0f).start()
+        ObjectAnimator.ofFloat(binding.optionOneContainer, "translationY", 0f).start()
+
+        // Изменяем свойства контейнеров с помощью метода animate: меняем прозрачность 0 на 1 с
+        //длительностью в 300 миллисекунд и вешаем слушатель конца анимации.
+        binding.optionTwoContainer.animate()
+            .alpha(0f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    binding.optionTwoContainer.isClickable = false
+                    binding.optionOneContainer.setOnClickListener(null)
+                }
+            })
+
+        binding.optionOneContainer.animate()
+            .alpha(0f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    binding.optionOneContainer.isClickable = false
+                }
+            })
+
+        binding.transparentBackground.animate()
+            .alpha(0f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    binding.transparentBackground.isClickable = false
+                }
+            })
     }
 }
+
+
---
+++ app/src/main/java/com/shumikhin/onespaefoto/ui/animations/AnimationsActivityOld5.kt
@@ -0,0 +1,56 @@
+package com.shumikhin.onespaefoto.ui.animations
+
+import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.transition.*
+import com.shumikhin.onespaefoto.databinding.ActivityAnimationsEnlargeBinding
+import com.shumikhin.onespaefoto.databinding.ActivityAnimationsPathTransitionsBinding
+import com.shumikhin.onespaefoto.databinding.ActivityAnimationsShuffleBinding
+
+class AnimationsActivityOld5 : AppCompatActivity() {
+
+    private var _binding: ActivityAnimationsShuffleBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityAnimationsShuffleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //В методе createViews мы программно создаём TextView и добавляем в LinearLayout. Этот
+        //процесс вам должен быть знаком по первому курсу. В методе onCreate мы создаём список текста,
+        //который мы будем перемешивать. Далее в TransitionManager мы передаём контейнер, в котором
+        //нужно анимировать элементы. В нашем случае это LinearLayout, который содержит динамически
+        //изменяемый набор из TextView. Как только в контейнере меняется набор элементов,
+        //TransitionManager смотрит, совпадают ли уникальные id новых TextView со старыми. Если да, то
+        //происходит плавное перемещение старого элемента на новое место.
+        //Этот способ подходит не только для TextView, но и для любого другого виджета.
+        val titles: MutableList<String> = ArrayList()
+        for (i in 0..4) {
+            titles.add(String.format("Item %d", i + 1))
+        }
+        createViews(binding.transitionsContainer, titles)
+        binding.button.setOnClickListener {
+            TransitionManager.beginDelayedTransition(binding.transitionsContainer,
+                ChangeBounds())
+            titles.shuffle()
+            createViews(binding.transitionsContainer, titles)
+        }
+    }
+    private fun createViews(layout: ViewGroup, titles: List<String>) {
+        layout.removeAllViews()
+        for (title in titles) {
+            val textView = TextView(this)
+            textView.text = title
+            textView.gravity = Gravity.CENTER_HORIZONTAL
+            ViewCompat.setTransitionName(textView, title)
+            layout.addView(textView)
+        }
+    }
+}
